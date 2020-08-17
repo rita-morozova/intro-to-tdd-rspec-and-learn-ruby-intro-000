48bd8fec8af712effaba3a0495a0513ad9ac0cd2
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-17 17:48:36 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,3 @@
-def current_age_for_birth_year
-   birth_year = 1984
-   current_year = 2020
-   current_age = current_year - birth_year
-   puts current_age
+def current_age_for_birth_year(birth_year)
+  2020 - birth_year
 end 
